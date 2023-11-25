530f8efd67aaa672fdceedfd044e5158444af332
Maksym <maks.gavlytskyy@gmail.com>
2023-11-25 21:07:52 +0300

    add kr

---
+++ kr/script.js
@@ -0,0 +1,13 @@
+function changeColor() {
+    var randomColor = getRandomColor();
+    document.getElementById('myDiv').style.backgroundColor = randomColor;
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
